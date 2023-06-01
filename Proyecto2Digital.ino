fdbff3b910cd5c05853852d4e2cae9ea0d7ec79d
JAVH0002002 <135205766+JAVH0002002@users.noreply.github.com>
2023-06-01 04:36:10 -0600

    Update Proyecto2Digital.ino

--- Proyecto2Digital.ino
+++ Proyecto2Digital.ino
@@ -17,12 +17,15 @@ int direccionY2 = A3; // Pin analógico A3
 int pulsador_guardar = 8;
 int pulsador_realizar = PD7;
 int pulsador_nuevo = PD6;
+int LadoaOtrob = 9;
+
 
 int lecturaX;
 int lecturaY;
 int lectura_pulsador_guardar;
 int lectura_pulsador_realizar;
 int lectura_pulsador_nuevo;
+int lectura_lado_a_otros;
 int lecturaX2;
 int lecturaY2;
 int lectura_pulsador_guardar2;
@@ -37,16 +40,31 @@ const int EEPROM_ADDR_SERVO1 = 0;
 const int EEPROM_ADDR_SERVO2 = 1;
 const int EEPROM_ADDR_SERVO3 = 2;
 const int EEPROM_ADDR_SERVO4 = 3;
+
 const int EEPROM_ADDR_SERVO5 = 4;
 const int EEPROM_ADDR_SERVO6 = 5;
 const int EEPROM_ADDR_SERVO7 = 6;
 const int EEPROM_ADDR_SERVO8 = 7;
+const int EEPROM_ADDR_SERVO9 = 8;
+
+const int EEPROM_ADDR_SERVO10 = 9;
+const int EEPROM_ADDR_SERVO11 = 10;
+const int EEPROM_ADDR_SERVO12 = 11;
+const int EEPROM_ADDR_SERVO13 = 12;
+const int EEPROM_ADDR_SERVO14 = 13;
+const int EEPROM_ADDR_SERVO15 = 14;
+const int EEPROM_ADDR_SERVO16 = 15;
+const int EEPROM_ADDR_SERVO17 = 16;
+
+
 
 volatile bool guardarPosicion = false;
 volatile bool realizarAccion = false;
 volatile bool realizarAccionNuevo = false;
 volatile bool guardarPosicion2 = false;
 volatile bool realizarAccion2 = false;
+volatile bool LadoaOtro = false;
+
 
 int posicion__servo = 90;
 int posicion__servo2 = 90;
@@ -82,7 +100,34 @@ void restaurarPosiciones()
   lcd.init();
   lcd.init();
 }
+void Lado_a_Otro1()
+{
+  EEPROM.write(EEPROM_ADDR_SERVO10,posicion_servo= 0);
+  EEPROM.write(EEPROM_ADDR_SERVO11, posicion_servo2=90);
+  EEPROM.write(EEPROM_ADDR_SERVO12, posicion_servo3=90);
+  EEPROM.write(EEPROM_ADDR_SERVO13, posicion_servo4=0);
 
+  posicion_servo = EEPROM.read(EEPROM_ADDR_SERVO10);
+  posicion_servo2 = EEPROM.read(EEPROM_ADDR_SERVO11);
+  posicion_servo3 = EEPROM.read(EEPROM_ADDR_SERVO12);
+  posicion_servo4 = EEPROM.read(EEPROM_ADDR_SERVO13);
+
+  delay(1000);
+}
+void Lado_a_Otro2()
+{
+
+  EEPROM.write(EEPROM_ADDR_SERVO14,posicion_servo= 90);
+  EEPROM.write(EEPROM_ADDR_SERVO15, posicion_servo2=0);
+  EEPROM.write(EEPROM_ADDR_SERVO16, posicion_servo3=0);
+  EEPROM.write(EEPROM_ADDR_SERVO17, posicion_servo4=90);
+ 
+  posicion_servo = EEPROM.read(EEPROM_ADDR_SERVO14);
+  posicion_servo2 = EEPROM.read(EEPROM_ADDR_SERVO15);
+  posicion_servo3 = EEPROM.read(EEPROM_ADDR_SERVO16);
+  posicion_servo4 = EEPROM.read(EEPROM_ADDR_SERVO17);
+  delay(1000);
+}
 void handleGuardarPosicion()
 {
   guardarPosicion = true;
@@ -113,10 +158,12 @@ void setup()
   pinMode(pulsador_guardar, INPUT_PULLUP);
   pinMode(pulsador_realizar, INPUT_PULLUP);
   pinMode(pulsador_nuevo, INPUT_PULLUP);
+  pinMode(LadoaOtrob, INPUT_PULLUP);\
 
   attachInterrupt(digitalPinToInterrupt(pulsador_guardar), handleGuardarPosicion, FALLING);
   attachInterrupt(digitalPinToInterrupt(pulsador_realizar), handleRealizarAccion, FALLING);
   attachInterrupt(digitalPinToInterrupt(pulsador_nuevo), handleRealizarAccionNuevo, FALLING);
+  attachInterrupt(digitalPinToInterrupt(LadoaOtrob), handleRealizarAccionNuevo, FALLING);
 
 
   restaurarPosiciones();
@@ -136,7 +183,7 @@ void loop()
   lectura_pulsador_guardar = !digitalRead(pulsador_guardar);
   lectura_pulsador_realizar = !digitalRead(pulsador_realizar);
   lectura_pulsador_nuevo = !digitalRead(pulsador_nuevo);
-
+  lectura_lado_a_otros = !digitalRead(LadoaOtrob);
   if (lecturaX >= 550)
   {
     posicion_servo++;
@@ -262,6 +309,28 @@ void loop()
     delay(1000); // Espera un segundo para evitar ejecutar múltiples veces
   }
 
+  if (lectura_lado_a_otros)
+  {
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Lado A Lado         ");
+    lcd.setCursor(0, 1);
+    lcd.print("           ");
+    Lado_a_Otro1();
+    servomotor.write(posicion_servo);
+    servomotor2.write(posicion_servo2);
+    servomotor3.write(posicion_servo3);
+    servomotor4.write(posicion_servo4);
+    delay(1000);
+    Lado_a_Otro2();
+    servomotor.write(posicion_servo);
+    servomotor2.write(posicion_servo2);
+    servomotor3.write(posicion_servo3);
+    servomotor4.write(posicion_servo4);
+
+    delay(2000); // Espera un segundo para evitar guardar múltiples veces
+  }
+
   servomotor2.write(posicion_servo2);
   servomotor4.write(posicion_servo4);
   delay(10);
@@ -271,3 +340,4 @@ void loop()
   lcd.setCursor(0, 1);
   lcd.print("           ");
 }
+
